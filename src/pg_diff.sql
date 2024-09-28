841e72fea6749a0d88dddc91b70a3d15edcda798
Alex Yatkevich <ayatkevich@users.noreply.github.com>
2024-09-28 17:17:18 +0700

    move name and namespace to the top
    remove unnecessary fields from data

--- src/pg_diff.sql
+++ src/pg_diff.sql
@@ -1,6 +1,8 @@
 create type "pg_diff_record" as (
   "kind" text,
   "type" text,
+  "name" text,
+  "namespace" text,
   "data" jsonb
 );
 
@@ -8,27 +10,20 @@ create view "pg_diff_inspect" as (
   select
       null as "kind",
       'pg_class' as "type",
+      relName as "name",
+      relNamespace::regNamespace::text as "namespace",
       jsonb_build_object(
-        'name', relName,
-        'namespace', relNamespace::regNamespace,
         'type', relType::regType,
         'ofType', relOfType::regType,
         'owner', relOwner::regRole,
         'accessMethod', relAM, -- TODO get name
         'fileNode', relFileNode, -- TODO get name
         'tableSpace', relTableSpace, -- TODO get name
-        'hasIndex', relHasIndex,
         'isShared', relIsShared,
         'persistence', relPersistence,
         'kind', relKind,
-        'numberOfUserColumns', relNatTs,
-        'numberOfChecks', relChecks,
-        'hasRules', relHasRules,
-        'hasTriggers', relHasTriggers,
-        'hasSubClass', relHasSubclass,
         'rowSecurity', relRowSecurity,
         'forceRowSecurity', relForceRowSecurity,
-        'isPopulated', relIsPopulated,
         'replicaIdentity', relReplIdent,
         'isPartition', relIsPartition,
         'acl', relAcl,
@@ -40,13 +35,13 @@ create view "pg_diff_inspect" as (
 create function "pg_diff" (jsonb)
 returns setof "pg_diff_record"
 language sql as $$
-  select '-' as "kind", "type", "data" from (
+  select '-' as "kind", "type", "name", "namespace", "data" from (
     select * from jsonb_populate_recordSet(null::"pg_diff_record", $1)
     except
     select * from "pg_diff_inspect"
   )
   union
-  select '+' as "kind", "type", "data" from (
+  select '+' as "kind", "type", "name", "namespace", "data" from (
     select * from "pg_diff_inspect"
     except
     select * from jsonb_populate_recordSet(null::"pg_diff_record", $1)
