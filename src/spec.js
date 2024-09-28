d7b9df5b5b7abafd342c5dd341890105bedc2d3e
Alex Yatkevich <ayatkevich@users.noreply.github.com>
2024-09-28 16:12:23 +0700

    add drop table test

--- src/spec.js
+++ src/spec.js
@@ -1,13 +1,16 @@
 import { PGlite } from "@electric-sql/pglite";
-import { afterAll, beforeAll, describe, expect, test } from "@jest/globals";
+import { afterEach, beforeEach, describe, expect, test } from "@jest/globals";
 import { readFile } from "fs/promises";
 
-const pg = new PGlite();
-const sql = async (...args) => (await pg.sql(...args)).rows;
-
 describe("pg-diff", () => {
-  beforeAll(async () => pg.exec(await readFile("./src/pg_diff.sql", "utf-8")));
-  afterAll(async () => pg.close());
+  let pg;
+  let sql;
+  beforeEach(async () => {
+    pg = await PGlite.create();
+    sql = async (...args) => (await pg.sql(...args)).rows;
+  });
+  beforeEach(async () => pg.exec(await readFile("./src/pg_diff.sql", "utf-8")));
+  afterEach(async () => pg.close());
 
   test("create table", async () => {
     const original = await sql`select * from "pg_diff_inspect"`;
@@ -18,6 +21,48 @@ describe("pg-diff", () => {
     expect(diff).toEqual([
       {
         kind: "+",
+        type: "pg_class",
+        data: {
+          acl: null,
+          kind: "r",
+          name: "test",
+          type: "test",
+          owner: "postgres",
+          ofType: "-",
+          options: null,
+          fileNode: "16393",
+          hasIndex: false,
+          hasRules: false,
+          isShared: false,
+          namespace: "public",
+          tableSpace: "0",
+          hasSubClass: false,
+          hasTriggers: false,
+          isPartition: false,
+          isPopulated: true,
+          persistence: "p",
+          rowSecurity: false,
+          accessMethod: "2",
+          numberOfChecks: 0,
+          replicaIdentity: "d",
+          forceRowSecurity: false,
+          numberOfUserColumns: 0,
+        },
+      },
+    ]);
+  });
+
+  test("drop table", async () => {
+    await sql`create table "test" ()`;
+
+    const original = await sql`select * from "pg_diff_inspect"`;
+
+    await sql`drop table "test"`;
+
+    const diff = await sql`select * from "pg_diff"(${original})`;
+    expect(diff).toEqual([
+      {
+        kind: "-",
         type: "pg_class",
         data: {
           acl: null,
