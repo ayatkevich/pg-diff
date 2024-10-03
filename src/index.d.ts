458e91606e3e76d55b403d8cb38e3826fac5c914
Alex Yatkevich <ayatkevich@users.noreply.github.com>
2024-10-03 14:58:50 +0700

    add typings

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,1 +1,25 @@
 export declare const definition: string;
+
+type SqlFunction = (
+  template: TemplateStringsArray,
+  ...substitutions: any[]
+) => Promise<Record<string, any>[]>;
+
+interface InspectionRecord {
+  kind: null;
+  type: string;
+  name: string;
+  namespace: string;
+  extras: Record<string, any>;
+}
+
+interface DiffRecord extends InspectionRecord {
+  kind: "+" | "-" | "+-";
+}
+
+export declare function inspect(sql: SqlFunction): Promise<InspectionRecord[]>;
+
+export declare function diff(
+  sql: SqlFunction,
+  input: { before: InspectionRecord[]; after: InspectionRecord[] }
+): Promise<DiffRecord[]>;
